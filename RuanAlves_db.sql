fed308b5bda8ae1558599b622608996afee7eb8b
Ruan Augusto Alves <ruanaugust.alves@gmail.com>
2025-02-25 21:03:36 -0300

    Banco do CRUID

---
+++ RuanAlves_db.sql
@@ -0,0 +1,10 @@
+create database RuanAlves_db
+
+create table usuario(
+	idusuario int not null auto_increment,
+	nome text,
+    telefone text,
+    usuario text,
+    senha text,
+    primary key (idusuario)
+);
